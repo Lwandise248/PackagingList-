57f7c8d774a21bbec64f9de255e916d5a2054b8f
Lwandise248 <ST10487940@myemeris.edu.za>
2026-06-09 20:37:54 +0200

    AddPackaging.kt
    Declarations

---
+++ app/src/main/java/com/example/packaginglist_/AddPackaging.kt
@@ -0,0 +1,5 @@
+    //Declarations
+    private lateinit var  tvTitle : TextView
+    private lateinit var btnAddItem : Button
+    private lateinit var btnScreenTwo : Button
+    private lateinit var btnExit : Button
