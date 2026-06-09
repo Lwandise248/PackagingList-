a7b3e95276aaca3e1ed8a67963d3cbc65a35b8ac
Lwandise248 <ST10487940@myemeris.edu.za>
2026-06-09 20:52:40 +0200

    update AddPackaging.kt
    Typecasting

--- app/src/main/java/com/example/packaginglist_/AddPackaging.kt
+++ app/src/main/java/com/example/packaginglist_/AddPackaging.kt
@@ -3,3 +3,7 @@
     private lateinit var btnAddItem : Button
     private lateinit var btnScreenTwo : Button
     private lateinit var btnExit : Button
+        val btnAddItem = findViewById<Button>(R.id.btnAddItem)
+        val btnScreenTwo = findViewById<Button>(R.id.btnScreenTwo)
+        val btnExit = findViewById<Button>(R.id.btnExit)
+
