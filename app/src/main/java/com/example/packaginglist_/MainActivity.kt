da2e7f0d8d6bb31e2b603386db4d2277a390c4d0
Lwandise248 <ST10487940@myemeris.edu.za>
2026-06-09 11:47:09 +0200

    Update main activity
    Typecasting

--- app/src/main/java/com/example/packaginglist_/MainActivity.kt
+++ app/src/main/java/com/example/packaginglist_/MainActivity.kt
@@ -16,6 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        //Typecasting
+
+        btnStart = findViewById(R.id.btnStart)
+        PackagingList = findViewById(R.id.PackagingList)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
