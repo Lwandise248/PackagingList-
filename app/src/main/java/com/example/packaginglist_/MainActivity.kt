a0fcc9ee9d43808fe1c888cb8ebfea55537aa114
Lwandise248 <ST10487940@myemeris.edu.za>
2026-06-09 11:55:01 +0200

    Update Main Activity
    Btn set on click

--- app/src/main/java/com/example/packaginglist_/MainActivity.kt
+++ app/src/main/java/com/example/packaginglist_/MainActivity.kt
@@ -20,6 +20,11 @@ class MainActivity : AppCompatActivity() {
 
         btnStart = findViewById(R.id.btnStart)
         PackagingList = findViewById(R.id.PackagingList)
+
+        btnStart.setOnClickListener {
+            val intent = Intent(this, AddPackaging::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
